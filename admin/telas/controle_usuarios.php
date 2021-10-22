a949658702f55f6af39f753a667144c93c49aebe
SAwarm <jonasdasilvaelias@gmail.com>
2021-10-21 21:46:23 -0300

    tratamentos e correções na tela

--- admin/telas/controle_usuarios.php
+++ admin/telas/controle_usuarios.php
@@ -1,4 +1,116 @@
-<?php include_once("menu.php"); ?>
+<?php include_once("./menuAcesso/menuAuxiliar.php"); ?>
+
+                  
+                        <div class="col-md-12">
+                            <br>
+                            <div style="margin-bottom: 2%;">
+                                <a type="button" href="#" class="btn btn-info btn-fill btn-add-user" style="margin-right: 62.5%;">Adicionar Novo Usuário</a>
+                                    <select class="form-group" style="height: 50%;
+                                        padding: 8px 20px;
+                                        margin: 6px 0;
+                                        display: inline-block;
+                                        border: 1px solid #ccc;
+                                        box-sizing: border-box;">
+                                        <option>Coordenador(a)</option>
+                                        <option>Professor(a)</option>
+                                        <option>Auxiliar</option>
+                                        <option>Responsáveis</option>
+                                    </select>
+                                <a type="button" href="#" class="btn btn-info btn-filter">Filtrar</a>
+                            </div>
+                            <div class="card strpied-tabled-with-hover">
+                                <div class="card-header ">
+                                    <h4 class="card-title">Striped Table with Hover</h4>
+                                    <p class="card-category">Here is a subtitle for this table</p>
+                                </div>
+                                <div class="card-body table-full-width table-responsive">
+                                    <table class="table table-hover table-striped">
+                                        <thead>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Salary</th>
+                                            <th>Country</th>
+                                            <th>City</th>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Dakota Rice</td>
+                                                <td>$36,738</td>
+                                                <td>Niger</td>
+                                                <td>Oud-Turnhout</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Minerva Hooper</td>
+                                                <td>$23,789</td>
+                                                <td>Curaçao</td>
+                                                <td>Sinaai-Waas</td>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td>Sage Rodriguez</td>
+                                                <td>$56,142</td>
+                                                <td>Netherlands</td>
+                                                <td>Baileux</td>
+                                            </tr>
+                                            <tr>
+                                                <td>4</td>
+                                                <td>Philip Chaney</td>
+                                                <td>$38,735</td>
+                                                <td>Korea, South</td>
+                                                <td>Overland Park</td>
+                                            </tr>
+                                            <tr>
+                                                <td>5</td>
+                                                <td>Doris Greene</td>
+                                                <td>$63,542</td>
+                                                <td>Malawi</td>
+                                                <td>Feldkirchen in Kärnten</td>
+                                            </tr>
+                                            <tr>
+                                                <td>6</td>
+                                                <td>Mason Porter</td>
+                                                <td>$78,615</td>
+                                                <td>Chile</td>
+                                                <td>Gloucester</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+<div class="modal fade" id="modal-users" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Mensagem</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body message-modal" style="color: black">
+        <form>
+            <div class="form-group">
+                <label for="nome-user" class="col-form-label">Nome:</label>
+                <input type="text" class="form-control" id="nome-user" class="nome-user">
+            </div>
+            <div class="form-group">
+                <label for="message-text" class="col-form-label">Message:</label>
+                <input class="form-control" id="message-text"></input>
+            </div>
+        </form>
+        <br>
+      </div>
+      <div class="modal-footer">
+          <br>
+        <button type="button" class="btn btn-primary btn-fill btn-send-users" style="cursor: pointer;" data-dismiss="modal">Enviar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 <script src="../assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
@@ -18,3 +130,23 @@
 <script src="../assets/js/demo.js"></script>
 
 </html>
+
+<script>
+
+    $(window).resize(function () {
+        if($( document ).width() >= 950 ){
+            $('.nav-mobile-menu').hide();
+        }else{
+            $('.nav-mobile-menu').show();
+        }
+    })
+
+    $( ".btn-add-user" ).click(function() {
+        $('#modal-users').modal('show');
+    });
+
+    $( ".btn-filter" ).click(function() {
+        
+    })
+
+</script>
